d98b7545268486b65defb25d07226d2fb1b5b04a
Felipe Assis <felipe.assis.2002@gmail.com>
2025-01-18 22:18:21 -0300

    implementing movement

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -1,10 +1,24 @@
 using Input;
 using System;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerView : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float dashSpeed = 15f;
+    [SerializeField] private float dashDuration = 0.3f;
+    [SerializeField] private float dashCooldown = 2f;
+    [SerializeField] private float  maxFallVelocity = -10f;
+    [SerializeField] private float  maxUpVelocity = 50f;
+    [SerializeField] private float  jumpCooldown = 0.5f;
+    [SerializeField] private float  jumpUpwardsDuration = 0.3f;
+    [SerializeField] private float  jumpVelocity = 2f;
+    [SerializeField] private float  gravity = -5f;
+    [Header("Camera Settings")]
     [SerializeField] private float lookSensitivityX = 100f;
     [SerializeField] private float lookSensitivityY = 3f;
     [SerializeField] private float verticalLookUperBounds = 90f;
@@ -15,14 +29,58 @@ public class PlayerView : MonoBehaviour
 
     private PlayerInput playerInput;
     private Vector2 lookInput;
+    private Vector2 moveInput;
     private float xCameraRotation = 10f;
+    private bool isDashing = false;
+    private float dashCooldownTimer = 0f;
+    private float dashTimer = 0f;
+    private float jumpCooldownTimer = 0f;
+    private float jumpTimer = 0f;
+    private bool isJumping = false;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
         playerInput.Enable();
         playerInput.Player.Look.performed += ctx => LookInput(ctx.ReadValue<Vector2>());
+        playerInput.Player.Look.canceled += ctx => LookInput(ctx.ReadValue<Vector2>());
+        playerInput.Player.Move.performed += ctx => MoveInput(ctx.ReadValue<Vector2>());
+        playerInput.Player.Move.canceled += ctx => MoveInputStop(ctx.ReadValue<Vector2>());
+        playerInput.Player.Dash.performed += ctx => Dash();
+        playerInput.Player.Jump.performed += ctx => Jump();
         characterController = GetComponent<CharacterController>();
+    }
+
+    private void Jump()
+    {
+        if(characterController.isGrounded && jumpCooldownTimer <= 0)
+        {
+            isJumping = true;
+            jumpCooldownTimer = jumpCooldown;
+            jumpTimer = jumpUpwardsDuration;
+        }
+    }
+
+    private void Dash()
+    {
+        if (dashCooldownTimer > 0 || isDashing)
+        {
+            return;
+        }
+
+        isDashing = true;
+        dashCooldownTimer = dashCooldown;
+        dashTimer = dashDuration;
+    }
+
+    private void MoveInputStop(Vector2 vector2)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    private void MoveInput(Vector2 vector2)
+    {
+        moveInput = vector2.normalized;
     }
 
     private void Start()
@@ -33,7 +91,6 @@ public class PlayerView : MonoBehaviour
 
     private void LookInput(Vector2 vector2)
     {
-        Debug.Log(vector2.normalized);
         lookInput = vector2.normalized;
     }
 
@@ -59,7 +116,83 @@ public class PlayerView : MonoBehaviour
 
     private void Update()
     {
+        HandleDashLogic();
+        HandleJumpLogic();
         Look();
+        Move();
+    }
+
+    private void HandleJumpLogic()
+    {
+        if(jumpCooldownTimer > 0)
+        {
+            jumpCooldownTimer -= Time.deltaTime;
+        }
+
+        if (isJumping)
+        {
+            jumpTimer -= Time.deltaTime;
+
+            if(jumpTimer <= 0)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    private void HandleDashLogic()
+    {
+        if(dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+
+        if (isDashing)
+        {
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                isDashing = false;
+                dashTimer = 0f;
+            }
+        }
+    }
+
+    private float GetSpeed()
+    {
+        return isDashing ? dashSpeed : movementSpeed;
+    }
+
+    private void Move()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        if(isDashing && move == Vector3.zero)
+        {
+            move = transform.forward.normalized;
+        }
+
+        move *= GetSpeed() * Time.deltaTime;
+
+
+        if (characterController.isGrounded && characterController.velocity.y < 0)
+        {
+            move.y = -2f; // Small downward force to keep the character grounded
+        }
+     
+
+        if (!characterController.isGrounded && !isJumping)
+        {
+            move.y += (gravity + characterController.velocity.y) * Time.deltaTime;
+        }
+
+        if(isJumping)
+        {
+            move.y = jumpVelocity * Time.deltaTime;
+        }
+
+        move.y = Mathf.Clamp(move.y, maxFallVelocity, maxUpVelocity);
+        characterController.Move(move);
     }
 
     private void Look()
@@ -69,7 +202,5 @@ public class PlayerView : MonoBehaviour
         xCameraRotation = Mathf.Clamp(xCameraRotation, verticalLookLowerBounds, verticalLookUperBounds);
 
         cameraTransform.localRotation = Quaternion.Euler(xCameraRotation, 0f, 0f);
-
-        lookInput = Vector2.zero;
     }
 }
